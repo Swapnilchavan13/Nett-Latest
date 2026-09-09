45273824793086048d7ee95b5678eaaff127e04a
Swapnil Chavan <chavanswapnil822@gmail.com>
2026-09-09 12:00:45 +0530

    updated code

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    tailwindcss(), // Added Tailwind v4 plugin
+    react(),
+  ],
   resolve: {
     tsconfigPaths: true,
   },
   build: {
     outDir: 'build',
-    cssMinify: false, // Prevents Vite from calling missing CSS minifier modules
+    cssMinify: false,
   },
 })
